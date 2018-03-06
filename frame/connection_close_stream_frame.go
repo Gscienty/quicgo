9fe40ed6dad00862bb1e9e1a58e2d675baf06dd5
高小川 <gaoxiaochuan@hotmail.com>
2018-03-06 15:34:10 +0800

    add connection close stream frame

---
+++ frame/connection_close_stream_frame.go
@@ -0,0 +1,59 @@
+package frame
+
+import (
+	"bytes"
+	"errors"
+	"../utils"
+	"encoding/binary"
+)
+
+type ConnectionCloseStreamFrame struct {
+	errorCode	uint16
+	reason		string
+}
+
+func ConnectionCloseStreamFrameParse (b *bytes.Reader) (*ConnectionCloseStreamFrame, error) {
+	errcodeBuf := make ([]byte, 2)
+	readedLen, err := b.Read (errcodeBuf)
+	if err != nil {
+		return nil, err
+	}
+	if readedLen != 2 {
+		return nil, errors.New ("ConnectionCloseStreamFrameParse error: buffer too small")
+	}
+	errCode := binary.BigEndian.Uint16 (errcodeBuf)
+
+	reasonLen, err := utils.VarLenIntegerStructParse (b)
+	if err != nil {
+		return nil, err
+	}
+
+	reasonBuf := make ([]byte, reasonLen.GetVal ())
+	len, err := b.Read (reasonBuf)
+	if err != nil {
+		return nil, err
+	}
+	if len != int (reasonLen.GetVal ()) {
+		return nil, errors.New ("ConnectionCloseStreamFrameParse error: reason length error")
+	}
+
+	return &ConnectionCloseStreamFrame {
+		errorCode: errCode,
+		reason: string (reasonBuf),
+	}, nil
+}
+
+func (this *ConnectionCloseStreamFrame) Serialize (b *bytes.Buffer) error {
+	utils.BigEndian.WriteUInt (b, uint64 (this.errorCode), 2)
+	reasonBuf := []byte (this.reason)
+	reasonLen := utils.VarLenIntegerStructNew (uint64 (len (reasonBuf)))
+	_, err := reasonLen.Serialize (b)
+	if err != nil {
+		return err
+	}
+	_, err = b.Write (reasonBuf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
