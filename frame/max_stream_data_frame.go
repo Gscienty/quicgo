55749371672cc76a90ecf4abb5372715bf1aa146
高小川 <gaoxiaochuan@hotmail.com>
2018-03-06 16:03:01 +0800

    add max stream data frame

---
+++ frame/max_stream_data_frame.go
@@ -0,0 +1,42 @@
+package frame
+
+import (
+	"bytes"
+	"../utils"
+	"../protocol"
+)
+
+type MaxStreamDataFrame struct {
+	streamID	protocol.StreamID
+	maximumData	utils.VarLenIntegerStruct
+}
+
+func MaxStreamDataFrameParse (b *bytes.Reader) (*MaxStreamDataFrame, error) {
+	streamID, err := protocol.StreamIDParse (b)
+	if err != nil {
+		return nil, err
+	}
+
+	maximumData, err := utils.VarLenIntegerStructParse (b)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MaxStreamDataFrame {
+		streamID: *streamID,
+		maximumData: *maximumData,
+	}, nil
+}
+
+func (this *MaxStreamDataFrame) Serialize (b *bytes.Buffer) error {
+	_, err := this.streamID.Serialize (b)
+	if err != nil {
+		return err
+	}
+	_, err = this.maximumData.Serialize (b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
