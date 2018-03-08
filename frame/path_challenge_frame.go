d62f13a36ffce67d4ba33adc70be4a91f444cade
高小川 <gaoxiaochuan@hotmail.com>
2018-03-08 15:40:27 +0800

    fix draft 10: add path challenge&response frame

---
+++ frame/path_challenge_frame.go
@@ -0,0 +1,35 @@
+package frame
+
+import (
+	"errors"
+	"bytes"
+)
+
+type PathChallengeFrame struct {
+	Frame
+	data uint8
+}
+
+func PathChallengeFrameParse (b *bytes.Reader) (*PathChallengeFrame, error) {
+	frameType, err := b.ReadByte ()
+	if err != nil {
+		return nil, err
+	}
+	if frameType != FRAME_TYPE_PATH_CHALLENGE {
+		return nil, errors.New ("PathChallengeFrameParse error: frametype not equal 0x0e")
+	}
+	data, err := b.ReadByte ()
+	if err != nil {
+		return nil, err
+	}
+
+	return &PathChallengeFrame { Frame { frameType }, uint8 (data) }, nil
+}
+
+func (this *PathChallengeFrame) Serialize (b *bytes.Buffer) error {
+	err := b.WriteByte (this.frameType)
+	if err != nil {
+		return err
+	}
+	return b.WriteByte (this.data)
+}
---
+++ frame/path_response_frame.go
@@ -0,0 +1,35 @@
+package frame
+
+import (
+	"errors"
+	"bytes"
+)
+
+type PathResponseFrame struct {
+	Frame
+	data uint8
+}
+
+func PathResponseFrameParse (b *bytes.Reader) (*PathResponseFrame, error) {
+	frameType, err := b.ReadByte ()
+	if err != nil {
+		return nil, err
+	}
+	if frameType != FRAME_TYPE_PATH_RESPONSE {
+		return nil, errors.New ("PathResponseFrameParse error: frametype not equal 0x0e")
+	}
+	data, err := b.ReadByte ()
+	if err != nil {
+		return nil, err
+	}
+
+	return &PathResponseFrame { Frame { frameType }, uint8 (data) }, nil
+}
+
+func (this *PathResponseFrame) Serialize (b *bytes.Buffer) error {
+	err := b.WriteByte (this.frameType)
+	if err != nil {
+		return err
+	}
+	return b.WriteByte (this.data)
+}
