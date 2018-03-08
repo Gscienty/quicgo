8c130fa8eef023326294b5cb4defa31a2ee5ed4d
高小川 <gaoxiaochuan@hotmail.com>
2018-03-08 21:13:01 +0800

    add connection

---
+++ utils/connection.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"sync"
+	"net"
+)
+
+type IConnection interface {
+	Send ([]byte) error
+	Recv ([]byte) (int, net.Addr, error)
+	Close () error
+	GetLocalAddress () net.Addr
+	GetRemoteAddress () net.Addr
+	SetRemoteAddress (net.Addr)
+}
+
+type ConnectionBase struct {
+	remoteAddressLock	sync.RWMutex
+
+	conn				net.PacketConn
+	remoteAddress		net.Addr
+}
+
+var _ IConnection = &ConnectionBase { }
+
+func (this *ConnectionBase) Send (b []byte) error {
+	_, err := this.conn.WriteTo (b, this.remoteAddress)
+	return err
+}
+
+func (this *ConnectionBase) Recv (b []byte) (int, net.Addr, error) {
+	return this.conn.ReadFrom (b)
+}
+
+func (this *ConnectionBase) Close () error {
+	return this.conn.Close ()
+}
+
+func (this *ConnectionBase) GetLocalAddress () net.Addr {
+	return this.conn.LocalAddr ()
+}
+
+func (this *ConnectionBase) GetRemoteAddress () net.Addr {
+	this.remoteAddressLock.RLock ()
+	addr := this.remoteAddress
+	this.remoteAddressLock.RUnlock ()
+	return addr
+}
+
+func (this *ConnectionBase) SetRemoteAddress (addr net.Addr) {
+	this.remoteAddressLock.Lock ()
+	this.remoteAddress = addr
+	this.remoteAddressLock.Unlock ()
+}
